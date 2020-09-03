f9a7f17c498f3e4d20baa74bbd5b2532d772120e
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-09-03 14:33:36 +0800

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import PatientsList from './PatientsList'
 import PatientsInsert from './PatientsInsert'
 import PatientsUpdate from './PatientsUpdate'
+import GridExample from './RealTimeGrid'
 
-export { PatientsList, PatientsInsert, PatientsUpdate }
+export { PatientsList, PatientsInsert, PatientsUpdate, GridExample }
